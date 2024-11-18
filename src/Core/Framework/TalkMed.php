c14e63f6e40b882fef633c64e14dfc705b88a667
DDDzzz99 <dengze@dameng.com>
2024-11-18 09:50:17 +0800

    init

---
+++ src/Core/Framework/TalkMed.php
@@ -0,0 +1,21 @@
+<?php
+namespace Core\Framework;
+
+class TalkMed
+{
+    /**
+     * No explanation
+     *
+     * @author aug dai
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index ()
+    {
+        echo "This Is TalkMed";
+    }
+
+}
+
+?>
